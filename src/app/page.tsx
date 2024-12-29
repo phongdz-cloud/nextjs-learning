eb0172db51d96bd6e3d6d5e2822bfd5c1b88c699
phonghh <longtam20015@gmail.com>
2024-12-29 12:19:06 +0700

    Use Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="font-sans text-4xl font-normal text-center">
-        Xin chào mọi người
-      </h1>
-    </div>
+    <main>
+      <div className="w-[700px] h-[700px] bg-red-300">
+        <Image
+          src="https://images.pexels.com/photos/29076255/pexels-photo-29076255/free-photo-of-vibrant-indoor-market-scene-with-stalls.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="phonghh"
+          width={200}
+          height={200}
+          quality={100}
+        />
+      </div>
+    </main>
   );
 }
